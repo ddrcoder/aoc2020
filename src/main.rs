868879ccd301a6047de094439c676ecbc98d0906
Tom Jackson <ddrcoder@gmail.com>
2020-12-04 20:08:50 -0800

    day 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate scan_fmt;
+
 use std::collections::hash_set::HashSet;
 use std::env;
 use std::io;
@@ -16,10 +19,29 @@ fn day1() {
     }
 }
 
+fn day2() {
+    // 2-9 c: ccccccccc
+    let count = io::stdin()
+        .lock()
+        .lines()
+        .map(|line| line.ok().unwrap())
+        .filter(|s| {
+            let (min, max, ch_constrained, password) =
+                scan_fmt!(&s, "{}-{} {}: {}", usize, usize, char, String)
+                    .ok()
+                    .unwrap();
+            let reps = password.chars().filter(|ch| *ch == ch_constrained).count();
+            min <= reps && reps <= max
+        })
+        .count();
+    println!("{}", count);
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
     match args[1].parse().ok().unwrap() {
         1 => day1(),
+        2 => day2(),
         _ => panic!("Pick a day!"),
     }
 }

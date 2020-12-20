9d917178f164a190e3e79f4e2624059040f850e0
Tom Jackson <tjackson@fb.com>
2020-12-19 23:11:38 -0800

    day20

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-#![feature(bindings_after_at)]
 #[macro_use]
 extern crate scan_fmt;
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
 
 use std::collections::hash_map::HashMap;
 use std::collections::hash_set::HashSet;
@@ -12,6 +16,8 @@ use std::iter::once;
 use std::iter::Iterator;
 
 fn lines(filename: &str) -> Vec<String> {
+    let x = 3;
+
     BufReader::new(File::open(filename).ok().unwrap())
         .lines()
         .map(|line| line.ok().unwrap())
@@ -587,25 +593,25 @@ fn day12(gold: bool) -> usize {
         }
     };
     let start = ((0, 0), if gold { (10, 1) } else { (1, 0) });
-    let ((x, y), _) = lines("day12.txt")
-        .iter()
-        .fold(start, |(p @ (x, y), d @ (dx, dy)), line| {
-            let n = line[1..].parse().ok().unwrap();
-            match line.chars().next().unwrap() {
-                'F' => ((x + n * dx, y + n * dy), d),
-                'N' if gold => (p, (dx, dy + n)),
-                'E' if gold => (p, (dx + n, dy)),
-                'S' if gold => (p, (dx, dy - n)),
-                'W' if gold => (p, (dx - n, dy)),
-                'N' => ((x, y + n), d),
-                'E' => ((x + n, y), d),
-                'S' => ((x, y - n), d),
-                'W' => ((x - n, y), d),
-                'L' => (p, rotate(d, 360 - n)),
-                'R' => (p, rotate(d, n)),
-                _ => panic!(),
-            }
-        });
+    let ((x, y), _) = lines("day12.txt").iter().fold(start, |(p, d), line| {
+        let (x, y) = p;
+        let (dx, dy) = d;
+        let n = line[1..].parse().ok().unwrap();
+        match line.chars().next().unwrap() {
+            'F' => ((x + n * dx, y + n * dy), d),
+            'N' if gold => (p, (dx, dy + n)),
+            'E' if gold => (p, (dx + n, dy)),
+            'S' if gold => (p, (dx, dy - n)),
+            'W' if gold => (p, (dx - n, dy)),
+            'N' => ((x, y + n), d),
+            'E' => ((x + n, y), d),
+            'S' => ((x, y - n), d),
+            'W' => ((x - n, y), d),
+            'L' => (p, rotate(d, 360 - n)),
+            'R' => (p, rotate(d, n)),
+            _ => panic!(),
+        }
+    });
     (x.abs() + y.abs()) as usize
 }
 
@@ -887,8 +893,6 @@ fn day17(gold: bool) -> usize {
 }
 fn day18(gold: bool) -> usize {
     let lines = lines("day18.txt");
-    let mut ops = vec![];
-    let mut values = vec![];
     let mut sum = 0;
     fn eval(op: char, v1: i64, v2: i64) -> i64 {
         match op {
@@ -898,6 +902,13 @@ fn day18(gold: bool) -> usize {
         }
     }
 
+    let prec = |ch| match ch {
+        '*' => 3,
+        '+' => 2,
+        '(' | ')' => 1,
+        '!' => 0,
+        _ => panic!("{}", ch),
+    };
     for line in [
         "2 * 3 + (4 * 5)",
         "1 + (2 * 3) + (4 * (5 + 6))",
@@ -907,56 +918,42 @@ fn day18(gold: bool) -> usize {
         "9 * 8 + 2 + (4 * (2 * 2 + 9 * 2) * 9 * 3 * 8) + 8 * 5",
     ]
     .iter()
-    {}
-    for line in lines.iter() {
-        for ch in line.chars() {
+    {
+        let mut ops = vec![];
+        let mut values = vec![];
+        //{}
+        //for line in lines.iter() {
+        for ch in line.chars().chain(once('!')) {
             println!("{} {} {:?} {:?}", line, ch, values, ops);
             match ch {
                 '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
-                    let value = ch as i64 - '0' as i64;
-                    if let Some(op) = ops.pop() {
-                        if op == '(' {
-                            values.push(value);
-                            ops.push('(');
-                        } else if let Some(last) = values.pop() {
-                            values.push(eval(op, value, last));
-                        }
-                    } else {
-                        values.push(value);
-                    }
+                    values.push(ch as i64 - '0' as i64);
                 }
-                '*' | '+' | '(' => {
-                    ops.push(ch);
-                }
-                ')' => {
-                    while let Some(op) = ops.pop() {
-                        if op == '(' {
-                            if let Some(op) = ops.pop() {
-                                if op == '(' {
-                                    ops.push('(');
-                                    break;
-                                }
-                                let a = values.pop().unwrap_or_else(|| {
-                                    panic!("in {} v{:?} o{:?}", line, &values, &ops)
-                                });
-                                if let Some(b) = values.pop() {
-                                    values.push(eval(op, a, b));
-                                } else {
-                                    values.push(a);
-                                }
-                            }
+                '*' | '+' | '(' | ')' | '!' => {
+                    while let Some(op) = ops.last() {
+                        println!("{} {} {:?} {:?}", line, ch, values, ops);
+                        if prec(*op) < prec(ch) {
                             break;
                         }
-                        let a = values
-                            .pop()
-                            .unwrap_or_else(|| panic!("in {} v{:?} o{:?}", line, &values, &ops));
-                        let b = values
-                            .pop()
-                            .unwrap_or_else(|| panic!("in {} v{:?} o{:?}", line, &values, &ops));
-                        values.push(eval(op, a, b));
+                        match *op {
+                            '*' => {
+                                let a = values.pop().unwrap();
+                                let b = values.pop().unwrap();
+                                values.push(a * b);
+                            }
+                            '+' => {
+                                let a = values.pop().unwrap();
+                                let b = values.pop().unwrap();
+                                values.push(a + b);
+                            }
+                            '!' => {}
+                            '(' | ')' => {}
+                            _ => panic!("{}", *op),
+                        }
+                        ops.pop();
                     }
+                    ops.push(ch);
                 }
-
                 ' ' => {}
                 _ => panic!("{}??", ch),
             }
@@ -969,10 +966,179 @@ fn day18(gold: bool) -> usize {
 
     return sum as usize;
 }
+//#[derive(Parser)]
+//#[grammar = "../day19a.pest"]
+//struct Day19AParser;
+
+#[derive(Parser)]
+#[grammar = "../day19b.pest"]
+struct Day19BParser;
+
+fn day19(gold: bool) -> usize {
+    let lines = lines("day19.input");
+    lines
+        .iter()
+        .filter_map(|line| {
+            if gold {
+                Day19BParser::parse(Rule::r0, line)
+                    .ok()
+                    .map(|ps| (line, ps.as_str()))
+            } else {
+                Day19BParser::parse(Rule::r0, line)
+                    .ok()
+                    .map(|ps| (line, ps.as_str()))
+            }
+        })
+        .filter(|(line, rep)| line == rep)
+        .count()
+}
+
+fn day20(gold: bool) -> usize {
+    let input = content("day20.txt");
+    let tiles: HashMap<_, Vec<Vec<char>>> = input
+        .split("\n\n")
+        .map(|blocks| {
+            let mut lines = blocks.split("\n");
+            let num = scan_fmt!(lines.next().unwrap(), "Tile {}:", usize)
+                .ok()
+                .unwrap();
+            (num, lines.map(|str| str.chars().collect()).collect())
+        })
+        .collect();
+    let mut sigs = HashMap::new();
+    println!("#3691\n{:?}", tiles.get(&3691));
+
+    fn edge_sig(tile: &Vec<Vec<char>>, flip: bool, edge: u8) -> String {
+        /*
+              ->0
+           3
+           ^
+           |     |
+                 v
+                 1
+            2<-
+            opposite = (edge + 2) % 4
+            flip = transpose = counterclockwise from tl corner
+        */
+        assert_eq!(tile.len(), 10, "{:?}", tile);
+        assert_eq!(tile[9].len(), 10, "{:?}", tile);
+        dbg!(tile);
+        let sig: String = match if flip { 3 - edge } else { edge } {
+            0 => tile[0].iter().cloned().collect(),
+            1 => tile.iter().map(|r| r[9]).collect(),
+            2 => tile[9].iter().rev().cloned().collect(),
+            3 => tile.iter().rev().map(|r| r[0]).collect(),
+            _ => panic!(),
+        };
+        if flip {
+            sig.chars().rev().collect()
+        } else {
+            sig
+        }
+    };
+
+    for flip in &[false, true] {
+        for edge in 0..4 {
+            for (num, tile) in tiles.iter() {
+                sigs.entry(edge_sig(tile, *flip, edge))
+                    .or_insert(vec![])
+                    .push((*num, *flip, edge));
+            }
+        }
+    }
+    println!("Filtered:\n{:?}", sigs);
+    //let mut choices = vec![];
+
+    println!("Filtered into:\n{:?}", sigs,);
+    let mut choices = vec![];
+    fn find_tiles(
+        tiles: &HashMap<usize, Vec<Vec<char>>>,
+        choices: &mut Vec<(usize, bool, u8)>,
+        sigs: &HashMap<String, Vec<(usize, bool, u8)>>,
+    ) -> bool {
+        if choices.len() == 9 {
+            return true;
+        }
+        let sig_above = if choices.len() >= 3 {
+            let (id, flip, rot) = choices[choices.len() - 3];
+            // 0->1 1->0 2->3 3->2
+            Some(edge_sig(tiles.get(&id).unwrap(), flip, (5 - rot) % 4))
+        } else {
+            None
+        };
+        let sig_left = if choices.len() % 3 != 0 {
+            let (id, flip, rot) = choices[choices.len() - 1];
+            // 0->1 1->0 2->3 3->2
+            Some(edge_sig(tiles.get(&id).unwrap(), flip, (5 - rot) % 4))
+        } else {
+            None
+        };
+        let used = |id| choices.iter().any(|used| used.0 == id);
+        let get_options = |sig: &str, side: u8| {
+            sigs.get(sig)
+                .iter()
+                .flat_map(|options| options.iter().cloned())
+                .filter(|(id, _, _)| !used(*id))
+                //.cloned()
+                .map(|(id, flip, edge)| (id, flip, (4 + side - edge) % 4))
+                .collect::<Vec<_>>()
+        };
+        if let Some(sig_above) = &sig_above {
+            for choice in get_options(sig_above, 0) {
+                if let Some(sig_left) = &sig_left {
+                    continue;
+                    //???
+                }
+                choices.push(choice);
+                if find_tiles(tiles, choices, sigs) {
+                    return true;
+                }
+                choices.pop();
+            }
+        } else if let Some(sig_left) = &sig_left {
+            for choice in get_options(sig_left, 3) {
+                choices.push(choice);
+                if find_tiles(tiles, choices, sigs) {
+                    return true;
+                }
+                choices.pop();
+            }
+        }
+        println!();
+        for (id, flip, rot) in choices.iter() {
+            let tile = tiles.get(id).unwrap();
+            println!("F{}, R{}", flip, rot);
+            for line in tile {
+                println!("{}", line.iter().cloned().collect::<String>());
+            }
+        }
+        false
+    }
+    for (sig, edges) in &sigs {
+        if edges.len() < 2 {
+            continue;
+        }
+        for (id, flip, edge) in edges {
+            choices.push((
+                *id,
+                *flip,
+                (5 - *edge) % 4, // rotate it so this edge is on the right (edge 1)
+            ));
+            if find_tiles(&tiles, &mut choices, &sigs) {
+                assert_eq!(choices.len(), 9);
+                return choices[0].0 * choices[2].0 * choices[6].0 * choices[8].0;
+            }
+        }
+    }
+    panic!();
+}
 
 fn main() {
     let solutions = [
-        day18, //day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, // day11, day12, day13, day14, //day15, day16,
+        //day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, // day11, day12, day13, day14, //day15, day16,
+        //day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, // day11, day12, day13, day14, //day15, day16,
+        // da17, day18,day19
+        day20,
     ];
     for (i, solution) in solutions.iter().enumerate() {
         println!("{}: {}, {}", i + 1, solution(false), solution(true));
